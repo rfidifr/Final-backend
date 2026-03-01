29d809ccd9422a8f27818fa7f05e7f72f7054780
AKASHKATYAN <akashkatyan2020@gmail.com>
2026-03-01 09:54:11 +0530

    Making check health checkpont for checking databas

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
+from email.mime import text
+
 from fastapi import FastAPI, Depends, HTTPException, status
+from requests import Session
+from app.database import get_db
 from app.routers  import admin, manager,login
 app=FastAPI(
     title="Secure RFID Arcade Management System",
@@ -12,3 +16,19 @@ app.include_router(login.router)
 @app.get("/")
 def home():
     return {"message":"go to /docs"}
+
+@app.get("/health")
+def health_check(db: Session = Depends(get_db)):
+    try:
+        
+        db.execute(text("SELECT 1"))
+        return {
+            "status": "online",
+            "database": "connected",
+            "message": "FastAPI is talking to Render PostgreSQL!"
+        }
+    except Exception as e:
+        # This will catch connection timeouts or credential errors
+        raise HTTPException(
+            status_code=500, 
+            detail=f"Database connection failed: {str(e)}")    
